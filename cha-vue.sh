fc73fb48800c0fd3194ff5a71c2bd17176d6cd42
minusing <1776875119@qq.com>
2022-06-20 17:19:32 +0800

    修改端口号

--- cha-vue.sh
+++ cha-vue.sh
@@ -10,9 +10,9 @@ git pull origin master
 echo "打包最新代码"
 npm run build
 echo "开始构建镜像"
-docker build -t cha-vue:1.0 .
+docker build -t cha-vue:1.0.0 .
 echo "删除旧容器"
 docker stop cha-vue-container
 docker rm cha-vue-container
 echo "启动新容器"
-docker container run -p 81:80 -d --name cha-vue-container cha-vue:1.0
+docker container run -p 81:80 -d --name cha-vue-container cha-vue:1.0.0
