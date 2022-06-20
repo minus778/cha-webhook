a87dae5d02ba7869725b99de662cb5286e96a68a
minusing <1776875119@qq.com>
2022-06-20 18:35:30 +0800

    修改后端项目docker端口号

--- cha-node.sh
+++ cha-node.sh
@@ -8,9 +8,9 @@ git clean -f
 echo "拉取最新代码"
 git pull origin master
 echo "开始构建镜像"
-docker build -t cha-node:1.0 .
+docker build -t cha-node:1.0.0 .
 echo "删除旧容器"
 docker stop cha-node-container
 docker rm cha-node-container
 echo "启动新容器"
-docker container run -p 3001:3000 -d --name cha-node-container cha-node:1.0
+docker container run -p 3001:3001 -d --name cha-node-container cha-node:1.0.0
