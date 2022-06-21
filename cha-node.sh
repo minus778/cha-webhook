427144ff626d46add1e9aaa79c9d6df6864e9d6a
minusing <1776875119@qq.com>
2022-06-21 09:50:23 +0800

    docker构建前删除旧镜像

--- cha-node.sh
+++ cha-node.sh
@@ -7,10 +7,12 @@ git reset --hard origin/master
 git clean -f
 echo "拉取最新代码"
 git pull origin master
-echo "开始构建镜像"
-docker build -t cha-node:1.0.0 .
 echo "删除旧容器"
 docker stop cha-node-container
 docker rm cha-node-container
+echo "删除旧镜像"
+docker rmi cha-node:1.0.0
+echo "开始构建新镜像"
+docker build -t cha-node:1.0.0 .
 echo "启动新容器"
 docker container run -p 3001:3000 -d --name cha-node-container cha-node:1.0.0
--- cha-vue.sh
+++ cha-vue.sh
@@ -11,10 +11,12 @@ echo "下载项目依赖"
 cnpm install
 echo "打包项目"
 npm run build
-echo "开始构建镜像"
-docker build -t cha-vue:1.0.0 .
 echo "删除旧容器"
 docker stop cha-vue-container
 docker rm cha-vue-container
+echo "删除旧镜像"
+docker rmi cha-vue:1.0.0
+echo "开始构建新镜像"
+docker build -t cha-vue:1.0.0 .
 echo "启动新容器"
 docker container run -p 81:80 -d --name cha-vue-container cha-vue:1.0.0
